57b081effed882f5b7bf97faee5013a2375f6c68
0xNaim <naimislam3545@gmail.com>
2021-12-21 16:42:54 +0600

    site loading animation added

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,18 +1,51 @@
+import { Grid } from '@mui/material';
+import {
+  useEffect,
+  useState
+} from 'react';
 import {
   Route,
   Routes
 } from 'react-router-dom';
+import { HashLoader } from 'react-spinners';
 import Layout from '../components/layout/Layout';
 import Home from '../pages/home/Home';
 import Shortener from '../pages/shortener/Shortener';
 
+const App = () => {
+  const [loading, setLoading] = useState(true);
 
-const App = () => (
-  <Layout>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/short" element={<Shortener />} />
-    </Routes>
-  </Layout>
-);
+  useEffect(() => {
+    setLoading(false);
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <Grid
+          container
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          sx={{
+            minHeight: '100vh'
+          }}
+        >
+          <HashLoader
+            color="#36D7B7"
+            loading={loading}
+            size={50}
+          />
+        </Grid>
+      ) : (
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/short" element={<Shortener />} />
+          </Routes>
+        </Layout>
+      )}
+    </div>
+  );
+};
 export default App;
